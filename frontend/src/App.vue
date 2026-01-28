2ffe65e1f60c0b17d25e32972288942a6955590c
App Developer <app.dev@investmentor.com.au>
2026-01-28 20:41:05 +0300

    add vue

---
+++ frontend/src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import MyButton from './components/MyButton.vue'
+</script>
+
+<template>
+  <h1>Мой проект</h1>
+  <!-- Юзаем как обычный HTML-тег -->
+  <MyButton text="Нажми меня, блять" />
+</template>
